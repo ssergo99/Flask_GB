3b2a293b3187c495c0453a02dda0b8b424edfdee
ssergo99 <95188760+ssergo99@users.noreply.github.com>
2023-02-28 13:34:43 +0300

    DZ4

--- blog/models/user.py
+++ blog/models/user.py
@@ -4,9 +4,24 @@ from flask_login import UserMixin
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     is_staff = Column(Boolean, nullable=False, default=False)
+    email = Column(String(255), nullable=False, default="", server_default="")
+    roles = db.relationship('Role', secondary='user_roles')
 
     def __repr__(self):
         return f"<User #{self.id} {self.username!r}>"
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from blog.app import app, db
+from blog.models.user import Role
 
 if __name__ == "__main__":
     app.run(
@@ -16,9 +17,13 @@ def init_db():
 @app.cli.command("create-users")
 def create_users():
     from blog.models import User
+    admin_role = Role(name='Admin')
     admin = User(username="admin", is_staff=True)
     james = User(username="james")
+    newadmin1 = User(username='new_admin', is_staff=True)
+    newadmin1.roles = [admin_role, ]
     db.session.add(admin)
     db.session.add(james)
+    db.session.add(newadmin1)
     db.session.commit()
     print("done! created users:", admin, james)

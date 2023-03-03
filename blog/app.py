98e50b76d2416e5d0ddda2e402c764d1896116d2
ssergo99 <95188760+ssergo99@users.noreply.github.com>
2023-03-03 12:36:52 +0300

    DZ5

--- blog/app.py
+++ blog/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 from blog.views.auth import auth_app, login_manager
 from blog.views.users import users_app
 from blog.views.articles import articles_app
 from blog.models.database import db
 import os
 from flask_migrate import Migrate
+from blog.security import flask_bcrypt
 
 cfg_name = os.environ.get("CONFIG_NAME") or "DevConfig"
 
@@ -17,6 +20,7 @@ app.register_blueprint(auth_app, url_prefix="/auth")
 
 db.init_app(app)
 login_manager.init_app(app)
+flask_bcrypt.init_app(app)
 
 
 @app.route("/")

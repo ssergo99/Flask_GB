3378d199267c8eedc0130d210be08ae835ed1adf
ssergo99 <95188760+ssergo99@users.noreply.github.com>
2023-03-17 11:05:00 +0300

    DZ9

--- blog/configs.py
+++ blog/configs.py
@@ -13,11 +13,15 @@ class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "abcdefg123456"
     WTF_CSRF_ENABLED = True
+    OPENAPI_URL_PREFIX = '/api/swagger'
+    OPENAPI_SWAGGER_UI_PATH = '/'
+    OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
 
 
 class DevConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
 
 class TestingConfig(BaseConfig):
     TESTING = True
